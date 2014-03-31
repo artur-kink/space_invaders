b07566e9aba4bd852ac2d6f7519f2f0ecb42ae0f
HellButterfly33 <thomasleong3@gmail.com>
2014-03-31 16:48:35 -0400

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -2,115 +2,137 @@
 
 enemy::enemy(){
     dir = 1;
+    
+    animationTimer.reset();
 }
 
 void enemy::update(){
     x += dir*0.5;
-    if(dir > 0 && x > 180){
+    if(dir > 0 && x > 37){
         dir = -1;
-        y -= 24;
+        y -= 3;
     }
-    else if(dir < 0 && x < -180){
+    else if(dir < 0 && x < -37){
         dir = 1;
-        y -= 24;
+        y -= 3;
     }
 }
 
 void enemy::draw(){
-	glPushMatrix();
-		// scale the enemy size
-		glScalef(0.2, 0.2, 1);
-		
+	
+	if(animationTimer.elapsed() > 1.0){
+		animationTimer.reset();
+		frame = !frame;
+	}
+	if(frame){
 		// left side
 		glPushMatrix();
-			glTranslatef(x-10, y-2, 0);
-			glScalef(1, 3, 1);
+			glTranslatef(x-10/5.0, y-2/5.0, 0);
+			glScalef(0.2, 0.6, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x-8, y+1, 0);
-			glScalef(1, 2, 1);
+			glTranslatef(x-8/5.0, y+1/5.0, 0);
+			glScalef(0.2, 0.4, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x-6, y, 0);
-			glScalef(1, 5, 1);
+			glTranslatef(x-6/5.0, y, 0);
+			glScalef(0.2, 1.0, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x-6, y+8, 0);
+			glTranslatef(x-6/5.0, y+8/5.0, 0);
+			glScalef(0.2, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x-4, y, 0);
+			glTranslatef(x-4/5.0, y, 0);
+			glScalef(0.2, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x-4, y+5, 0);
-			glScalef(1, 2, 1);
+			glTranslatef(x-4/5.0, y+5/5.0, 0);
+			glScalef(0.2, 0.4, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		// body
 		glPushMatrix();
-			glTranslatef(x, y+1, 0);
-			glScalef(3, 4, 1);
+			glTranslatef(x, y+1/5.0, 0);
+			glScalef(0.6, 0.8, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		
 		// right side
 		glPushMatrix();
-			glTranslatef(x+10, y-2, 0);
-			glScalef(1, 3, 1);
+			glTranslatef(x+10/5.0, y-2.0/5.0, 0);
+			glScalef(0.2, 0.6, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+8, y+1, 0);
-			glScalef(1, 2, 1);
+			glTranslatef(x+8/5.0, y+1/5.0, 0);
+			glScalef(0.2, 0.4, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+6, y, 0);
-			glScalef(1, 5, 1);
+			glTranslatef(x+6/5.0, y, 0);
+			glScalef(0.2, 1.0, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+6, y+8, 0);
+			glTranslatef(x+6/5.0, y+8/5.0, 0);
+			glScalef(0.2, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+4, y, 0);
+			glTranslatef(x+4/5.0, y, 0);
+			glScalef(0.2, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+4, y+5, 0);
-			glScalef(1, 2, 1);
+			glTranslatef(x+4/5.0, y+5/5.0, 0);
+			glScalef(0.2, 0.4, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		
 		glPushMatrix();
-			glTranslatef(x-3, y-6, 0);
-			glScalef(2, 1, 1);
+			glTranslatef(x-3/5.0, y-6/5.0, 0);
+			glScalef(0.4, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
 		glPushMatrix();
-			glTranslatef(x+3, y-6, 0);
-			glScalef(2, 1, 1);
+			glTranslatef(x+3/5.0, y-6/5.0, 0);
+			glScalef(0.4, 0.2, 1);
 			glColor3f(1.0f, 0, 0);
 			draw::cube();
 		glPopMatrix();
-	glPopMatrix();
+		
+		// Eyes
+		glPushMatrix();
+			glTranslatef(x-4/5.0, y+2/5.0, 0);
+			glScalef(0.2, 0.2, 1);
+			glColor3f(0, 0, 0);
+			draw::cube();
+		glPopMatrix();
+		glPushMatrix();
+			glTranslatef(x+4/5.0, y+2/5.0, 0);
+			glScalef(0.2, 0.2, 1);
+			glColor3f(0, 0, 0);
+			draw::cube();
+		glPopMatrix();
+	}
 }
