337e6d9ec1986467a06e3ffeb85f4740404e74b3
HellButterfly33 <thomasleong3@gmail.com>
2014-04-01 15:33:26 -0400

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -7,8 +7,27 @@ player::player(){
 
 void player::draw(){
     glPushMatrix();
-        glTranslatef(x, y, 0);
-        glColor3f(1.0f, 1.0f, 1.0f);
-        draw::cube();
-    glPopMatrix();
+		glTranslatef(x, y + 14 / 5.0, 0);
+		glScalef(0.2, 0.2, 0.6);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		draw::cube();
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(x, y + 10 / 5.0, 0);
+		glScalef(0.6, 0.6, 0.6);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		draw::cube();
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(x, y + 6 / 5.0, 0);
+		glScalef(2.2, 0.2, 0.6);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		draw::cube();
+	glPopMatrix();
+	glPushMatrix();
+		glTranslatef(x, y, 0);
+		glScalef(2.6, 1.0, 0.6);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		draw::cube();
+	glPopMatrix();
 }
